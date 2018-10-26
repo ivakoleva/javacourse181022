18c1d59c6111cfb85a16b286068df7982e40ac90
Iva Koleva <iva.koleva@clearcode.bg>
2018-10-26 12:56:44 +0300

    concurrency locking mechanisms, reentrant strategies

--- src/main/java/bg/clearcode/javacoude181022/example12/OrganizationService.java
+++ src/main/java/bg/clearcode/javacoude181022/example12/OrganizationService.java
@@ -3,6 +3,7 @@ package bg.clearcode.javacoude181022.example12;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -12,7 +13,12 @@ import java.util.stream.IntStream;
  * a significant bit of leva.bg project.
  */
 public class OrganizationService {
-    private List<Organization> organizationList;
+    //Collections.synchronizedList(new ArrayList);
+    // CopyOnWriteArrayList - non-locking read, but copying on each write
+    final private List<Organization> organizationList;
+
+    private final Object monitor = new Object();
+    private final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
 
     public OrganizationService() {
         organizationList = IntStream.range(0, 100).boxed().map(i -> {
@@ -23,23 +29,67 @@ public class OrganizationService {
     }
 
     // MODIFYING
-    public synchronized boolean addOrganization(final Organization organization) {
+    public boolean addOrganization(final Organization organization) {
         assert organization != null;
-        return organizationList.add(organization);
+        //synchronized (monitor) {
+        Runner1.threadPrint("Adding organization: " + organization);
+        reentrantReadWriteLock.writeLock().lock();
+        try {
+            return organizationList.add(organization);
+        } finally {
+            reentrantReadWriteLock.writeLock().unlock();
+            Runner1.threadPrint("Organization added: " + organization);
+        }
+        //}
     }
 
-    public synchronized boolean removeOrganization(final Organization organization) {
+    public boolean removeOrganization(final Organization organization) {
         assert organization != null;
-        return organizationList.remove(organization);
+        //synchronized (monitor) {
+        Runner1.threadPrint("Removing organization: " + organization);
+        reentrantReadWriteLock.writeLock().lock();
+        try {
+            return organizationList.remove(organization);
+        } finally {
+            reentrantReadWriteLock.writeLock().unlock();
+            Runner1.threadPrint("Organization removed: " + organization);
+        }
+        //}
     }
 
     // SEARCH
-    public synchronized List<Organization> getOrganizationsByPredicate(final Predicate<Organization> predicate) {
-        return organizationList.stream().filter(predicate).collect(Collectors.toList());
+    public List<Organization> getOrganizationsByPredicate(final Predicate<Organization> predicate) {
+        Runner1.threadPrint("Start retrieving list of organizations");
+        reentrantReadWriteLock.readLock().lock();
+        try {
+            return organizationList.stream().filter(predicate).collect(Collectors.toList());
+        } finally {
+            try {
+                Thread.sleep(1000 * 3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            reentrantReadWriteLock.readLock().unlock();
+            Runner1.threadPrint("End retrieving list of organizations");
+        }
     }
 
-    public synchronized Optional<Organization> getOrganizationFirstOrderedByComparatorFilteredByPredicate(final Comparator<Organization> comparator,
+    public Optional<Organization> getOrganizationFirstOrderedByComparatorFilteredByPredicate(final Comparator<Organization> comparator,
                                                                                              final Predicate<Organization> predicate) {
-        return organizationList.stream().sorted(comparator).filter(predicate).findFirst();
+        Runner1.threadPrint("Start retrieving organization");
+        reentrantReadWriteLock.readLock().lock();
+        try {
+            return organizationList.stream().sorted(comparator).filter(predicate).findFirst();
+        } finally {
+            try {
+                Thread.sleep(1000 * 3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            reentrantReadWriteLock.readLock().unlock();
+            Runner1.threadPrint("End retrieving organization");
+        }
     }
+
+
 }
--- src/main/java/bg/clearcode/javacoude181022/example12/Runner1.java
+++ src/main/java/bg/clearcode/javacoude181022/example12/Runner1.java
@@ -3,6 +3,10 @@ package bg.clearcode.javacoude181022.example12;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 /**
  * Created by Iva Koleva, ClearCode Ltd. on 26.10.2018,
@@ -10,6 +14,12 @@ import java.util.Optional;
  */
 public class Runner1 {
     private static final OrganizationService organizationService = new OrganizationService();
+
+    private static final Predicate<Organization> organizationNamePredicate = organization -> {
+        final String organizationName = organization.getName();
+        final int value = organizationName.charAt(organizationName.length() - 1);
+        return value == 0 || value % 2 != 0;
+    };
 
     public static void main(final String[] args) {
         final Runnable runnableModifying = () -> {
@@ -36,10 +46,8 @@ public class Runner1 {
 
         final Runnable runnableReading = () -> {
             System.out.println(Thread.currentThread().getName() + ": getting list of even names");
-            final List<Organization> organizationListEvenNames = organizationService.getOrganizationsByPredicate(organization -> {
-                final int value = Integer.valueOf(organization.getName().split("_")[1]);
-                return value == 0 || value % 2 != 0;
-            });
+            final List<Organization> organizationListEvenNames =
+                    organizationService.getOrganizationsByPredicate(organizationNamePredicate);
             System.out.println(Thread.currentThread().getName() + ": list of even names:");
             organizationListEvenNames.forEach(System.out::println);
             try {
@@ -51,10 +59,7 @@ public class Runner1 {
             System.out.println(Thread.currentThread().getName() + ": getting first even name");
             final Optional<Organization> organizationOptional = organizationService.getOrganizationFirstOrderedByComparatorFilteredByPredicate(
                     Comparator.comparing(Organization::getName),
-                    organization -> {
-                        final int value = Integer.valueOf(organization.getName().split("_")[1]);
-                        return value == 0 || value % 2 != 0;
-                    }); // todo: extract predicate for reuse
+                    organizationNamePredicate);
             System.out.println(Thread.currentThread().getName() + ": first even name: " +
                     (organizationOptional.isPresent() ? organizationOptional.get() : "no such organization found."));
             try {
@@ -64,15 +69,27 @@ public class Runner1 {
             }
         };
 
-        // TODO: wrap it up
-        /*final ExecutorService executorService = Executors.newFixedThreadPool(10);
-        executorService.execute(() -> System.out.println("hi!"));
+        final ExecutorService executorService = Executors.newFixedThreadPool(10);
+        IntStream.range(0, 100).forEach(i -> {
+            executorService.execute(runnableReading);
+            executorService.execute(runnableModifying);
+        });
+
+        // recommended using private monitors (implementation-specific)
+        /*synchronized (organizationService) {
+            System.out.println("taking the monitor from main thread");
+            while (true) {
+            }
+        }*/
+
         try {
             Thread.sleep(1000 * 6);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
+    }
 
-
+    public static void threadPrint(final String message) {
+        System.out.println(Thread.currentThread().getName() + ": " + message);
     }
 }
